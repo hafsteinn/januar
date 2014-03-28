1cdde8cf015d208e3efc592e2479d90432274305
hafsteinn <hafsteinn11@ru.is>
2014-03-28 11:50:23 +0000

    fixed round time logix and flexbox

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,36 +1,36 @@
 'use strict';
 
 angular.module('januarApp')
-  .controller('MainCtrl',
-  ['$scope', 'loggerFactory',
-   function ($scope, loggerFactory) {
+	.controller('MainCtrl',
+	['$scope', 'loggerFactory',
+	function ($scope, loggerFactory) {
 
-   	var start,end;
+	var start,end;
 
-   	$scope.submit = function(){
+	$scope.submit = function(){
 
-   		//calculate and round time
-   		var milliseconds = Math.abs(new Date(start) - new Date(end));
-   		var diff = milliseconds / 1000 / 60;
-   		var time = (Math.floor(diff/15) * 15);
+				//calculate and round time
+				var milliseconds = Math.abs(new Date(end) - new Date(start));
+				var minutes = milliseconds / 1000 / 60;
+				var quarters = Math.floor(minutes / 15);
 
-   		//add item to DB and retreive updated array
-   		loggerFactory.addEntry($scope.title, $scope.description, start, end,time/60);
-   		$scope.loggedProjects = loggerFactory.getMockDB();
+				//add item to DB and retreive updated array
+				loggerFactory.addEntry($scope.title, $scope.description, start, end,quarters/4);
+				$scope.loggedProjects = loggerFactory.getMockDB();
 
-   		//get statistics
-   		$scope.numberOfItems = $scope.loggedProjects.length;
-   		$scope.totalTime = 0;
-   		$scope.loggedProjects.forEach(function(entry){
-   			$scope.totalTime = $scope.totalTime + entry.total;
-   			console.log(entry);
-   		});
+				//get statistics
+				$scope.numberOfItems = $scope.loggedProjects.length;
+				$scope.totalTime = 0;
+				$scope.loggedProjects.forEach(function(entry){
+					$scope.totalTime = $scope.totalTime + entry.total;
+					console.log(entry);
+				});
 
-   		//clear
-   		$scope.title = "";
-   		$scope.description = "";
-   		$scope.date = null;
-   	};
+				//clear
+				$scope.title = '';
+				$scope.description = '';
+				$scope.date = null;
+			};
 
 	$('#startDateInput').datetimepicker({
 		onChangeDateTime:function(dp,$input){
@@ -44,4 +44,4 @@ angular.module('januarApp')
 		}
 	});
 
-  }]);
+}]);
