04420d34050719fd03551e6bdbf1903cbb02833d
hafsteinn <hafsteinn11@ru.is>
2014-03-28 12:07:57 +0000

    added DB initialization for viewing purposes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,27 @@ angular.module('januarApp')
 
 	var start,end;
 
+	//start by initializing the database for viewing purposes
+	//START INIT
+		loggerFactory.addEntry(
+			'The first project',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			null,
+			null,
+			10
+		);
+		loggerFactory.addEntry(
+			'The second project',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+			null,
+			null,
+			25
+		);
+
+		$scope.loggedProjects = loggerFactory.getMockDB();
+		stats();
+	//END INIT
+
 	$scope.submit = function(){
 
 				//calculate and round time
@@ -19,18 +40,21 @@ angular.module('januarApp')
 				$scope.loggedProjects = loggerFactory.getMockDB();
 
 				//get statistics
-				$scope.numberOfItems = $scope.loggedProjects.length;
-				$scope.totalTime = 0;
-				$scope.loggedProjects.forEach(function(entry){
-					$scope.totalTime = $scope.totalTime + entry.total;
-					console.log(entry);
-				});
+				stats();
 
 				//clear
 				$scope.title = '';
 				$scope.description = '';
 				$scope.date = null;
 			};
+
+	function stats(){
+				$scope.numberOfItems = $scope.loggedProjects.length;
+				$scope.totalTime = 0;
+				$scope.loggedProjects.forEach(function(entry){
+					$scope.totalTime = $scope.totalTime + entry.total;
+				});
+	};
 
 	$('#startDateInput').datetimepicker({
 		onChangeDateTime:function(dp,$input){
